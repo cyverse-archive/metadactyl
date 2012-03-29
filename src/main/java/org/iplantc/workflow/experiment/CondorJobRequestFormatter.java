d1b11363865d6ab2430fcebc4710333e581711a7
Dennis Roberts <dennis@iplantcollaborative.org>
2012-03-29 16:42:28 -0700

    CORE-3050: don't format output properties with negative order values

--- src/main/java/org/iplantc/workflow/experiment/CondorJobRequestFormatter.java
+++ src/main/java/org/iplantc/workflow/experiment/CondorJobRequestFormatter.java
@@ -620,7 +620,7 @@ public class CondorJobRequestFormatter implements JobRequestFormatter {
     }
 
     protected JSONObject formatOutputProperty(Property property, String value) {
-        if (property.getDataObject().isImplicit()) {
+        if (property.getDataObject().isImplicit() || property.getOrder() < 0) {
             return null;
         }
 
