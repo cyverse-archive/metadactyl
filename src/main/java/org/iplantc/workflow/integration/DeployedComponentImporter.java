6b9f0d1006bf7ffdc7587b38f11da4e82c071b8b
Dennis Roberts <dennis@iplantcollaborative.org>
2012-04-05 13:07:11 -0700

    CORE-3087: modify the deployed component import service so that it matches existing deployed components based on either ID or name and location

--- src/main/java/org/iplantc/workflow/integration/DeployedComponentImporter.java
+++ src/main/java/org/iplantc/workflow/integration/DeployedComponentImporter.java
@@ -13,9 +13,9 @@ import org.json.JSONObject;
 /**
  * Used to import deployed components from JSON objects. Each JSON object used to describe a deployed component must
  * contain "name" and "type" attributes. Other attributes that may be specified are "id", "location", "description",
- * "version" and "attribution". If the "id" attribute is not provided an identifier will be generated. The rest of
- * the optional attributes are left null if not specified.
- * 
+ * "version" and "attribution". If the "id" attribute is not provided an identifier will be generated. The rest of the
+ * optional attributes are left null if not specified.
+ *
  * @author Dennis Roberts
  */
 public class DeployedComponentImporter implements ObjectImporter {
@@ -44,7 +44,7 @@ public class DeployedComponentImporter implements ObjectImporter {
      * @param registry the new registry.
      */
     public void setRegistry(HeterogeneousRegistry registry) {
-        this.registry = registry = registry == null ? new NullHeterogeneousRegistry() : registry;
+        this.registry = registry == null ? new NullHeterogeneousRegistry() : registry;
     }
 
     /**
@@ -65,7 +65,7 @@ public class DeployedComponentImporter implements ObjectImporter {
 
     /**
      * Initializes the properties of a new deployed component importer instance.
-     * 
+     *
      * @param componentDao the deployed component DAO.
      */
     public DeployedComponentImporter(DeployedComponentDao componentDao) {
@@ -74,7 +74,7 @@ public class DeployedComponentImporter implements ObjectImporter {
 
     /**
      * Imports a deployed component using values from the given JSON object.
-     * 
+     *
      * @param json the JSON object describing the deployed component.
      * @throws JSONException if the JSON object is missing a required attribute.
      */
@@ -82,16 +82,17 @@ public class DeployedComponentImporter implements ObjectImporter {
     public void importObject(JSONObject json) throws JSONException {
         TitoDeployedComponentUnmarshaller unmarshaller = new TitoDeployedComponentUnmarshaller();
         DeployedComponent newComponent = unmarshaller.fromJson(json);
-        saveOrUpdate(newComponent);
+        saveOrUpdate(newComponent, json.optString("id", null));
     }
 
     /**
      * Either saves a new deployed component or updates an existing one.
-     * 
+     *
      * @param component deployed component to save or update.
+     * @param specifiedId the identifier specified in the import JSON.
      */
-    private void saveOrUpdate(DeployedComponent component) {
-        DeployedComponent existingComponent = findExistingComponent(component);
+    private void saveOrUpdate(DeployedComponent component, String specifiedId) {
+        DeployedComponent existingComponent = findExistingComponent(component, specifiedId);
         if (existingComponent == null) {
             componentDao.save(component);
             registerDeployedComponent(component);
@@ -109,11 +110,12 @@ public class DeployedComponentImporter implements ObjectImporter {
 
     /**
      * Updates an existing deployed component in the database.
-     * 
+     *
      * @param component the new deployed component information.
      * @param existingComponent the existing deployed component.
      */
     private void updateExistingComponent(DeployedComponent component, DeployedComponent existingComponent) {
+        existingComponent.setName(component.getName());
         existingComponent.setLocation(component.getLocation());
         existingComponent.setAttribution(component.getAttribution());
         existingComponent.setDescription(component.getDescription());
@@ -124,18 +126,20 @@ public class DeployedComponentImporter implements ObjectImporter {
 
     /**
      * Finds an existing deployed component with the same name as the given deployed component.
-     * 
+     *
      * @param newComponent the deployed component to search for.
+     * @param specifiedId the deployed component identifier specified in the import JSON.
      * @return the deployed component or null if a matching deployed component isn't found.
      */
-    private DeployedComponent findExistingComponent(DeployedComponent newComponent) {
-        String name = newComponent.getName();
-        return componentDao.findUniqueInstanceByName(name);
+    private DeployedComponent findExistingComponent(DeployedComponent newComponent, String specifiedId) {
+        return specifiedId == null
+                ? componentDao.findByNameAndLocation(newComponent.getName(), newComponent.getLocation())
+                : componentDao.findById(specifiedId);
     }
 
     /**
      * Adds the given deployed component to the registry if there is a registry.
-     * 
+     *
      * @param deployedComponent the deployed component to register.
      */
     private void registerDeployedComponent(DeployedComponent deployedComponent) {
@@ -144,7 +148,7 @@ public class DeployedComponentImporter implements ObjectImporter {
 
     /**
      * Imports multiple deployed components from the given JSON array.
-     * 
+     *
      * @param array the JSON array describing the list of deployed components.
      * @throws JSONException if any of the JSON objects are missing required attributes.
      */
