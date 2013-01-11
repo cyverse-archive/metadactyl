4f3ddf46acd475542e98e98d34da354cd7d909fd
Dennis Roberts <dennis@iplantcollaborative.org>
2013-01-10 18:00:20 -0700

    CORE-3879: removed some dead code

--- src/main/sql/migration-scripts/00_step_precedence_inbound.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE step_precedence_inbound
-      ADD COLUMN id uuid NOT NULL,
-      DROP CONSTRAINT step_precedence_inbound_pkey,
-      ADD CONSTRAINT step_precedence_inbound_pkey PRIMARY KEY (id),
-      ADD CONSTRAINT id_step_precedence_id_unique UNIQUE (id, step_precedence_id),
-      DROP COLUMN hid;
