3010963d5b2ba4e0c717e55a2113ccaf874051b7
Dennis Roberts <dennis@iplantcollaborative.org>
2013-02-19 17:35:22 -0700

    CORE-3988: generate an analysis ID if someone attempts to import an analysis with an empty ID

--- src/main/java/org/iplantc/workflow/integration/json/TitoAnalysisUnmarshaller.java
+++ src/main/java/org/iplantc/workflow/integration/json/TitoAnalysisUnmarshaller.java
@@ -30,7 +30,7 @@ import org.json.JSONObject;
 
 /**
  * Used to convert a JSON document representing an analysis to an analysis.
- * 
+ *
  * @author Dennis Roberts
  */
 public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<TransformationActivity> {
@@ -44,7 +44,7 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
      * The registry of named objects.
      */
     private HeterogeneousRegistry registry;
-    
+
     private TitoIntegrationDatumMashaller integrationDatumUnmarshaller;
     private WorkspaceInitializer workspaceInitializer;
 
@@ -60,7 +60,7 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
 
     /**
      * Converts a JSON object to an analysis.
-     * 
+     *
      * @param json the JSON object.
      * @return the analysis.
      * @throws JSONException if the JSON object we receive is invalid.
@@ -68,7 +68,7 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
     @Override
     public TransformationActivity fromJson(JSONObject json) throws JSONException {
         TransformationActivity analysis = new TransformationActivity();
-        
+
         analysis.setId(getId(json, "analysis_id", "id"));
         analysis.setName(json.getString("analysis_name"));
         analysis.setDescription(json.getString("description"));
@@ -99,7 +99,7 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
 	/**
 	 * Gets the integration datum to use for this analysis.  If a matching integration datum already exists then that
 	 * one will be used.  Otherwise, a new one will be created.
-	 * 
+	 *
 	 * @param json the JSON object representing the analysis.
 	 * @return the matching integration datum.
 	 * @throws JSONException if a JSON error occurs.
@@ -121,7 +121,7 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
 
     private Set<TransformationActivityReference> getReferences(JSONObject input) throws JSONException {
         Set<TransformationActivityReference> results = new HashSet<TransformationActivityReference>();
-        
+
         if(input.has("references")) {
             JSONArray references = input.getJSONArray("references");
             for (int i = 0; i < references.length(); i++) {
@@ -131,14 +131,14 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
                 results.add(ref);
             }
         }
-        
+
         return results;
     }
 
     /**
      * Gets an identifier from the given JSON object. If the identifier isn't specified or is set to "auto-gen" then a
      * new one will be generated.
-     * 
+     *
      * @param json the JSON object describing the analysis that is being imported.
      * @param fieldNames the acceptable names of the field containing the identifier.
      * @return the identifier.
@@ -146,7 +146,7 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
      */
     private String getId(JSONObject json, String... fieldNames) throws JSONException {
         String id = JsonUtils.nonEmptyOptString(json, null, fieldNames);
-        if (id == null || id.equals("auto-gen")) {
+        if (StringUtils.isBlank(id) || id.equals("auto-gen")) {
             id = ImportUtils.generateId();
         }
         return id;
@@ -154,7 +154,7 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
 
     /**
      * Converts a JSON array to a list of mappings.
-     * 
+     *
      * @param analysis the analysis that is currently being unmarshalled.
      * @param array the JSON array.
      * @return the list of mappings.
@@ -176,7 +176,7 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
 
     /**
      * Converts a JSON array to a list of transformation steps.
-     * 
+     *
      * @param array the JSON array.
      * @return the list of transformation steps.
      * @throws JSONException if the JSON array is invalid.
@@ -191,7 +191,7 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
 
     /**
      * Converts a JSON object to a transformation step.
-     * 
+     *
      * @param json the JSON object.
      * @return the transformation step.
      * @throws JSONException if the JSON object is invalid.
@@ -207,7 +207,7 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
 
     /**
      * Generates a transformation from values in a JSON object that describes a transformation step.
-     * 
+     *
      * @param json the JSON object.
      * @return the transformation.
      * @throws JSONException if the JSON object is invalid.
@@ -218,19 +218,19 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
         validateTemplateId(templateId);
         transformation.setName("");
         transformation.setTemplate_id(templateId);
-        
+
         Map<String, String> valuesMap = propertyValueMapFromTransformationStepJson(json.getJSONObject("config"));
-        
+
         for(String key : valuesMap.keySet()) {
             transformation.addPropertyValue(key, valuesMap.get(key));
         }
-        
+
         return transformation;
     }
 
     /**
      * Gets the template identifier for the given transformation step.
-     * 
+     *
      * @param json the JSON object representing the transformation step.
      * @return the template identifier.
      * @throws JSONException if the template identifier can't be found.
@@ -250,7 +250,7 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
 
     /**
      * Gets the identifier of the template with the given name.
-     * 
+     *
      * @param templateName the name of the template.
      * @return the template ID or null if the template can't be found.
      */
@@ -261,7 +261,7 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
 
     /**
      * Validates a template identifier.
-     * 
+     *
      * @param templateId the template identifier.
      */
     private void validateTemplateId(String templateId) {
@@ -272,7 +272,7 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
 
     /**
      * Determines whether or not a template with the given template ID is in the registry.
-     * 
+     *
      * @param templateId the template identifier.
      * @return true if the template is in the registry.
      */
@@ -291,7 +291,7 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
 
     /**
      * Determines whether or not a template with the given template ID is in the database.
-     * 
+     *
      * @param templateId the template identifier.
      * @return true if the template is in the database.
      */
@@ -301,7 +301,7 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
 
     /**
      * Converts a JSON object to a property value map.
-     * 
+     *
      * @param json the JSON object.
      * @return the property value map.
      * @throws JSONException if the JSON object is invalid.
@@ -320,28 +320,28 @@ public class TitoAnalysisUnmarshaller implements TitoUnmarshaller<Transformation
 
     public Set<Rating> unmarshalRatings(JSONObject json) throws JSONException {
         Set<Rating> result = new HashSet<Rating>();
-        
+
         try {
             JSONArray ratingsArray = json.getJSONArray("ratings");
-        
+
             for (int i = 0; i < ratingsArray.length(); i++) {
                 JSONObject jsonRating = ratingsArray.getJSONObject(i);
-            
+
                 Rating rating = new Rating();
                 rating.setRaiting(jsonRating.getInt("rating"));
-            
+
                 // Grab the user's workspace for a User record.  Note that this
                 // will create the user and workspace if they don't already exist.
                 String username = jsonRating.getString("username");
                 Workspace workspace = workspaceInitializer.getWorkspace(daoFactory, username);
-            
+
                 rating.setUser(workspace.getUser());
                 result.add(rating);
             }
         } catch(JSONException jsonException) {
             // Rating data may not have been included.  Ignore
         }
-        
+
         return result;
     }
 
